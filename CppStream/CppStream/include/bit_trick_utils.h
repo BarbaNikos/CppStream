02c45a49b5558012c2c27e669f2b2f205f289700
NickKatsip <nick.katsip@gmail.com>
2016-11-22 17:27:37 -0500

    added the breaking down of projects and tests

--- CppStream/CppStream/include/bit_trick_utils.h
+++ CppStream/CppStream/include/bit_trick_utils.h
@@ -45,20 +45,17 @@ inline uint64_t __mvc_ctz_64(uint64_t value)
 class BitWizard
 {
 public:
-	static const uint8_t MultiplyDeBruijnBitPosition2[32];
+	static uint8_t max_uint8(uint8_t x, uint8_t y) { return x ^ ((x ^ y) & -(x < y)); }
+	static uint16_t max_uint16(uint16_t x, uint16_t y) { return x ^ ((x ^ y) & -(x < y)); }
+	static uint32_t max_uint32(uint32_t x, uint32_t y) { return x ^ ((x ^ y) & -(x < y)); }
+	static uint64_t max_uint64(uint64_t x, uint64_t y) { return x ^ ((x ^ y) & -(x < y)); }
 
-	static uint8_t max_uint8(uint8_t x, uint8_t y);
-	static uint16_t max_uint16(uint16_t x, uint16_t y);
-	static uint32_t max_uint32(uint32_t x, uint32_t y);
-	static uint64_t max_uint64(uint64_t x, uint64_t y);
-
-	static uint8_t min_uint8(uint8_t x, uint8_t y);
-	static uint16_t min_uint16(uint16_t x, uint16_t y);
-	static uint32_t min_uint32(uint32_t x, uint32_t y);
-	static uint64_t min_uint64(uint64_t x, uint64_t y);
+	static uint8_t min_uint8(uint8_t x, uint8_t y) { return y ^ ((x ^ y) & -(x < y)); }
+	static uint16_t min_uint16(uint16_t x, uint16_t y) { return y ^ ((x ^ y) & -(x < y)); }
+	static uint32_t min_uint32(uint32_t x, uint32_t y) { return y ^ ((x ^ y) & -(x < y)); }
+	static uint64_t min_uint64(uint64_t x, uint64_t y) { return y ^ ((x ^ y) & -(x < y)); }
 
 	static uint32_t log_base_2_of_power_of_2_uint(uint32_t value);
-	static uint64_t log_base_2_of_power_of_2_uint(uint64_t value);
 
 	static uint32_t swap_bits_32(uint32_t value);
 
@@ -69,17 +66,17 @@ public:
 	static uint16_t lowest_order_bit_index_slow(uint16_t value);
 	static uint32_t lowest_order_bit_index_slow(uint32_t value);
 	static uint64_t lowest_order_bit_index_slow(uint64_t value);
-	
+
 	static uint8_t highest_order_bit_index_slow(uint8_t value);
 	static uint16_t highest_order_bit_index_slow(uint16_t value);
 	static uint32_t highest_order_bit_index_slow(uint32_t value);
 	static uint64_t highest_order_bit_index_slow(uint64_t value);
-	
+
 	static uint8_t lowest_order_bit_index(uint8_t value);
 	static uint16_t lowest_order_bit_index(uint16_t value);
 	static uint32_t lowest_order_bit_index(uint32_t value);
 	static uint64_t lowest_order_bit_index(uint64_t value);
-	
+
 	static uint8_t highest_order_bit_index(uint8_t value);
 	static uint16_t highest_order_bit_index(uint16_t value);
 	static uint32_t highest_order_bit_index(uint32_t value);
@@ -96,54 +93,12 @@ public:
 };
 #endif // !BIT_TRICK_UTILS_H_
 
-inline uint8_t BitWizard::max_uint8(uint8_t x, uint8_t y)
-{
-	return x ^ ((x ^ y) & -(x < y)); // max(x, y)
-}
-
-inline uint16_t BitWizard::max_uint16(uint16_t x, uint16_t y)
-{
-	return x ^ ((x ^ y) & -(x < y)); // max(x, y)
-}
-
-inline uint32_t BitWizard::max_uint32(uint32_t x, uint32_t y)
-{
-	return x ^ ((x ^ y) & -(x < y)); // max(x, y)
-}
-
-inline uint64_t BitWizard::max_uint64(uint64_t x, uint64_t y)
-{
-	return x ^ ((x ^ y) & -(x < y)); // max(x, y)
-}
-
-inline uint8_t BitWizard::min_uint8(uint8_t x, uint8_t y)
-{
-	return y ^ ((x ^ y) & -(x < y)); // min(x, y)
-}
-
-inline uint16_t BitWizard::min_uint16(uint16_t x, uint16_t y)
-{
-	return y ^ ((x ^ y) & -(x < y)); // min(x, y)
-}
-
-inline uint32_t BitWizard::min_uint32(uint32_t x, uint32_t y)
-{
-	return y ^ ((x ^ y) & -(x < y)); // min(x, y)
-}
-
-inline uint64_t BitWizard::min_uint64(uint64_t x, uint64_t y)
-{
-	return y ^ ((x ^ y) & -(x < y)); // min(x, y)
-}
-
 inline uint32_t BitWizard::log_base_2_of_power_of_2_uint(uint32_t value)
 {
-	return value == 0 ? 0 : BitWizard::MultiplyDeBruijnBitPosition2[(uint32_t)(value * 0x077CB531U) >> 27];
-}
-
-inline uint64_t BitWizard::log_base_2_of_power_of_2_uint(uint64_t value)
-{
-	return uint64_t();
+	const uint8_t MultiplyDeBruijnBitPosition2[32] = {
+		0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
+		31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9 };
+	return value == 0 ? 0 : MultiplyDeBruijnBitPosition2[(uint32_t)(value * 0x077CB531U) >> 27];
 }
 
 inline uint32_t BitWizard::swap_bits_32(uint32_t v)
@@ -332,53 +287,49 @@ inline uint64_t BitWizard::highest_order_bit_index(uint64_t value)
 
 inline uint16_t BitWizard::lowest_order_bit_index_arch(uint16_t value)
 {
-	/*unsigned long mask;
-	return _BitScanForward(&mask, value) != 0 ? uint16_t(1) << mask : uint16_t(0);*/
-	return tzcnt16(value);
+	uint16_t s = 0x0001;
+	uint16_t tz = tzcnt16(value);
+	return value != 0 ? s << tz : 0x0000;
 }
 
 inline uint32_t BitWizard::lowest_order_bit_index_arch(uint32_t value)
 {
-	/*unsigned long mask;
-	return _BitScanForward(&mask, value) != 0 ? uint32_t(1) << mask : uint32_t(0);*/
-	return tzcnt32(value);
+	uint32_t s = 0x00000001;
+	uint32_t tz = tzcnt32(value);
+	return value != 0 ? s << tz : 0x00000000;
 }
 
 inline uint64_t BitWizard::lowest_order_bit_index_arch(uint64_t value)
 {
-	/*unsigned long mask;
-	return _BitScanForward64(&mask, value) != 0 ? uint64_t(1) << mask : uint64_t(0);*/
-	return tzcnt64(value);
+	uint64_t s = 0x0000000000000001;
+	uint64_t tz = tzcnt64(value);
+	return value != 0 ? s << tz : 0x0000000000000000;
 }
 
 inline uint8_t BitWizard::highest_order_bit_index_arch(uint8_t value)
 {
 	uint8_t s = 0x80;
 	uint8_t lz = lzcnt8(value);
-	return lz != 0 ? s >> lz : 0x00;
-	// return value != 0 ? s >> lzcnt8(value) : 0x00;
+	return value != 0 ? s >> lz : 0x00;
 }
 
 inline uint16_t BitWizard::highest_order_bit_index_arch(uint16_t value)
 {
 	uint16_t s = 0x8000;
-	uint16_t lz = lzcnt16(16);
-	return lz != 0 ? s >> lz : 0x0000;
-	// return value != 0 ? s >> lzcnt16(value) : 0x0000;
+	uint16_t lz = lzcnt16(value);
+	return value != 0 ? s >> lz : 0x0000;
 }
 
 inline uint32_t BitWizard::highest_order_bit_index_arch(uint32_t value)
 {
 	uint32_t s = 0x80000000;
 	uint32_t lz = lzcnt32(value);
-	return lz != 0 ? s >> lz : 0x00000000;
-	// return value != 0 ? s >> lzcnt32(value) : 0x00000000;
+	return value != 0 ? s >> lz : 0x00000000;
 }
 
 inline uint64_t BitWizard::highest_order_bit_index_arch(uint64_t value)
 {
 	uint64_t s = 0x8000000000000000;
 	uint64_t lz = lzcnt64(value);
-	return lz != 0 ? s >> lz : 0x0000000000000000;
-	// return value != 0 ? s >> lzcnt64(value) : 0x0000000000000000;
+	return value != 0 ? s >> lz : 0x0000000000000000;
 }
