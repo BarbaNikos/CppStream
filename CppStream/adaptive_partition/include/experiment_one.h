19e87995fe20561a54ca3bce0fed401df592ed19
Nick R. Katsipoulakis <nick.katsip@gmail.com>
2017-01-30 09:32:32 -0500

    started updating the make files

---
+++ CppStream/adaptive_partition/include/experiment_one.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef EXPERIMENT_ONE_H_
+#define EXPERIMENT_ONE_H_
+namespace AdaptivePartitionExperiment
+{
+	class ExperimentOne
+	{
+	private:
+		int BATCH_SIZE;
+		int WORKER_NUM;
+
+	public:
+
+	};
+}
+#endif
--- CppStream/adaptive_partition/src/main.cpp
+++ CppStream/adaptive_partition/src/main.cpp
@@ -7,7 +7,16 @@
 
 int main(int argc, char** argv)
 {
-	std::vector<uint64_t> stream;
-	StreamGenerator::generate_lognormal_stream(10, 5, stream);
+	std::vector<int> stream;
+	try
+	{
+		//StreamGenerator::generate_normal_stream(100000, 1000, stream);
+		StreamGenerator::generate_lognormal_stream(100000, 1000, stream);
+	} catch(const std::exception& e)
+	{
+		std::cout << e.what();
+	}
+	
+	StreamGenerator::generate_histogram(stream);
 	return 0;
 }
